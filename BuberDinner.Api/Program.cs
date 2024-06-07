dc46f883482a83ea7b85d10f196c0fc29eb34e88
Tommy Tran <tblyduc2412@gmail.com>
2024-06-07 16:50:30 +0700

    Part 1: Project setup

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,12 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
 }
 
 var app = builder.Build();
